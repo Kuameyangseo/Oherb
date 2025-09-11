1f005fdc01ea9ee7ce2f0885f6fb4bec3fd936d8
Kuameyangseo <iamyangseo@gmail.com>
2025-09-11 01:45:20 +0000

    first commit

--- apps/api-gateway/webpack.config.js
+++ apps/api-gateway/webpack.config.js
@@ -4,27 +4,22 @@ const { join, resolve } = require('path');
 module.exports = {
   output: {
     path: join(__dirname, 'dist'),
-    ...(process.env.NODE_ENV !== 'production' && {
-      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-    }),
-    resolve: {
+  },
+  resolve: {
       alias:{
         "@packages":resolve(__dirname,'../../packages'),
       },
       extensions:[".ts",".js"]
-    }
-  },
+    },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
-      sourceMaps: true,
     })
   ],
 };
